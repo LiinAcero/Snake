e03c5739da2ece48c71c713c945023a32491a740
Naty <langova97@gmail.com>
2023-02-17 22:16:06 +0100

    cleaned up your code

---
+++ Extras.cs
@@ -0,0 +1,44 @@
+﻿namespace Snake
+{
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    public class Circle
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Circle()
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+
+    public class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static int Speed { get; set; }
+        public int Score { get; set; }
+        public int Points { get; set; }
+        public bool GameOver { get; set; }
+        public Direction Direction { get; set; }
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Speed = 20;
+            Score = 0;
+            Points = 100;
+            GameOver = false;
+            Direction = Direction.Right;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,95 +1,36 @@
-using System.Drawing.Text;
-
 namespace Snake
 {
-     class Circle
-    {
-        public int x { get; set; }
-        public int y { get; set; }
-
-        public Circle()
-        {
-            x = 0;
-            y = 0;
-        }
-    }
     public partial class Snake : Form
     {
-        private const int GameBoardWidth = 20;
-        private const int GameBoardHeight = 20;
-        private const int SnakeSize = 10;
-        private const int FoodSize = 10;
-        private readonly Random _random = new Random();
-
-        private readonly List<Point> _snake = new List<Point>();
-        private Point _food;
-
-        private int _score;
-        private bool _gameOver;
+        private readonly Random random = new Random();
 
         private List<Circle> SnakeBod = new List<Circle>();
         private Circle food = new Circle();
-        private void startGame()
-        {
-
-        }
-        private void movePlayer() {
-        }
-
-        private void genFood()
-        {
-
-        }
-
-        private void eat()
-        {
-
-        }
-
-        private void die()
-        {
-
-        }
-
-        private void pictureBox_Paint(object sender, PaintEventArgs e)
-        {
-            Rectangle ee = new Rectangle(10, 10, 30, 30);
-            using (Pen pen = new Pen(Color.Black, 2))
-            {
-                e.Graphics.DrawRectangle(pen, ee);
-            }
-        }
-
-        public enum Direction
-        {
-            Left,
-            Right,
-            Up,
-            Down
-        }
-
-        private Direction _direction = Direction.Right;
+        private Settings settings = new Settings();
 
         public Snake()
         {
             InitializeComponent();
+        }
 
-            const int Rows = 10;
-            const int Cols = 10;
+        private void startGame()
+        {
+        }
 
-            int[,] gameBoard = new int[Rows, Cols];
+        private void movePlayer()
+        {
+        }
 
-            gameBoard[0, 0] = 1;
+        private void generateFood()
+        {
+        }
 
-            
+        private void eat()
+        {
+        }
 
-            
-
-
-            
-
-
-            
+        private void die()
+        {
         }
 
         private void Form1_KeyDown_1(object sender, KeyEventArgs e)
@@ -97,27 +38,21 @@ namespace Snake
             switch (e.KeyCode)
             {
                 case Keys.Left:
-                    _direction = Direction.Left;
+                    settings.Direction = Direction.Left;
                     break;
 
                 case Keys.Right:
-                    _direction = Direction.Right;
+                    settings.Direction = Direction.Right;
                     break;
 
                 case Keys.Up:
-                    _direction = Direction.Up;
-
+                    settings.Direction = Direction.Up;
                     break;
 
                 case Keys.Down:
-                    _direction = Direction.Down;
+                    settings.Direction = Direction.Down;
                     break;
             }
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -127,8 +62,6 @@ namespace Snake
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            
         }
-
     }
 }
